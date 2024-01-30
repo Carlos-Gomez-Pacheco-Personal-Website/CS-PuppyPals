24629cc52b7ee157e03119adf9ab7027dc61e95c
Carlos Gomez <carlosegomez001@hotmail.com>
2024-01-29 22:39:43 -0500

    files

--- Puppy-Pals/src/App.jsx
+++ Puppy-Pals/src/App.jsx
@@ -9,29 +9,25 @@ function App() {
   const featuredPup = puppies.find(pup => pup.id === featPupId);
   
   function handleClick(puppy) {
+    console.log("puppy id: ", puppy.id);
     setFeatPupId(puppy.id);
   }
-
   return (
-    <div className="App">
-      {puppies.map((puppy) => {
-        return (
-          <p onClick={() => handleClick(puppy)} key={puppy.id}>
-            {puppy.name}
-          </p>
-        );
-      })}
-      {featPupId && (
-        <div>
-          <h2>{featuredPup.name}</h2>
-          <ul>
-            <li>Age: {featuredPup.age}</li>
-            <li>Email: {featuredPup.email}</li>
-          </ul>
-        </div>
-      )}
+    <div className='App'>
+      {
+        puppies.map((puppy) => {
+          return <p onClick={() => handleClick(puppy)} key={puppy.id}>{puppy.name}</p>
+        })
+      }
+      {featPupId && <div>
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+      </div>}
     </div>
-  );
+  )
 }
 
 export default App
